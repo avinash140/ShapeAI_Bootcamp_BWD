a5a4cb9cdf19f62f75fd44eaf64afbc67d219ee4
avinash140 <85446176+avinash140@users.noreply.github.com>
2021-06-06 20:09:52 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  var curryeer = new Date (). getFullYear();
+  
+  return (
+    <Footer>
+      <p>Copyright @ 2021 </p>
+      </Footer>
+  );
+}
+
+export default Footer ;
+
+
